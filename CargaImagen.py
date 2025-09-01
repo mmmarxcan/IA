687d57122cfa729adfea6c167d6f30d463dfda89
unknown <mmmarxcan@gmail.com>
2025-09-01 13:30:36 -0600

    blur en imagen

--- CargaImagen.py
+++ CargaImagen.py
@@ -5,9 +5,12 @@ img = cv2.imread("imagen.jpg")
 img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 hist = cv2.calcHist([gray], [0], None , [256],[0,256])
+_, binary = cv2.threshold(gray,128,255,cv2.THRESH_BINARY)
+blur = cv2.GaussianBlur(gray, (5,5), 0)
 
 
-"""plt.imshow(img_rgb)
+
+"""
 plt.title("Imagen Original")
 plt.axis("off")
 plt.show()
@@ -21,4 +24,14 @@ plt.show()
 
 """plt.plot(hist)
 plt.title("Histograma")
+plt.show()"""
+
+"""plt.imshow(binary, cmap='gray')
+plt.title("Escala de grises")
+plt.axis("off")
+plt.show()"""
+
+"""plt.imshow(blur, cmap='gray')
+plt.title("Imagen Difuminada")
+plt.axis("off")
 plt.show()"""
